373e2733cabc9f22afc179aa77ff1b74b83e14a0
Muhammad Quwais Safutra <44535941+Muhammad-Quwais-Safutra@users.noreply.github.com>
2019-01-06 16:36:08 +0700

    Create username_detection.c
    buat orang jail pasti nunggu fitur ini muehe. kalian akan mengetahui apa username dari situs ataupun Host yang anda lacak

---
+++ HJCrew/HJCrew 1.0.3/Windows 32/modules/Scanner/username_detection.c
@@ -0,0 +1,23 @@
+/*	AUTHOR : MUHAMMAD QUWAIS SAFUTRA	*/
+
+#include <FELLOW.h>
+#include <TROJAN.h>
+
+/* anda musti menginstall modul di atas */
+
+void Scan(){
+	*__asm __NULL __based1 (*81.0 i) /
+	*__asm __NULL __based1 __cdeclspec (*91.2) i) /
+	*__asm __NULL __cdecl *HOST  (*51.1) /
+	end;
+}
+
+printf("====> USER      : ");
+printf("====> SERVER	: ");
+[asctime **Scan (NULL != import)]
+
+int main(){
+	setup->(*sel);
+	setup->(*self);
+	setup->(*ofp);
+}
