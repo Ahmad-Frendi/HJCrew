f30ee9e0fef0d4ea3c2385d025d22d7de2fb242b
Muhammad Quwais Safutra <44535941+Muhammad-Quwais-Safutra@users.noreply.github.com>
2019-01-06 16:30:55 +0700

    Create Os_detection.c

---
+++ HJCrew/HJCrew 1.0.3/Windows 32/modules/Scanner/Os_detection.c
@@ -0,0 +1,18 @@
+/*	AUTHOR : MUHAMMAD QUWAIS SAFUTRA	*/
+
+#include <SFML/Network.hpp>
+/* anda memerlukan SFML untuk menjalankan Modul Ini ! */
+
+int main(){
+	sfml _getdcwd * OS;
+	sfml.Network::swid* _getdcwd * DETECTION;
+	OS_DETECTION_(host (*strcat(char *s1) *) );
+	if(OS!=NULL){
+		printf("OS    : ",OS);
+		printf("TARGET: ",host);
+	} else {
+		tolower(int c)-[
+		
+	}
+	return 0;
+}
